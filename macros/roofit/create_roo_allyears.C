74d34595f41debb37f5b02f85d9a7389ad62e352
Anna Benecke <anna.benecke@cern.ch>
2022-04-02 17:17:53 +0200

    all years mode

---
+++ macros/roofit/create_roo_allyears.C
@@ -0,0 +1,51 @@
+#include "CreateRooWorkspace.h"
+
+int main(int argc, char *argv[])
+{
+  using namespace std;
+
+  bool dodata = true;
+  bool all_bkgds = true;
+
+  //  TString MA = "99999";
+  TString MA = "125";
+
+  std::vector<TString> categories = {"chi2h_2", "catma90","catma175","catma300"};
+  if(MA=="75") categories = {"catma60", "catma90","catma300"};
+  if(MA=="100") categories = {"catma90","catma300","chi2h_2"};
+  if(MA=="125") categories = {"catma90","catma300","chi2h_2"};
+  if(MA=="175") categories = {"chi2h_2","catma175","catma300"};
+  if(MA=="200") categories = {"catma175","catma300","chi2h_2"};
+  if(MA=="250") categories = {"catma175","catma300"};
+  if(MA=="350") categories = {"catma175","catma300"};
+  if(MA=="450") categories = {"catma175","catma300"};
+  if(MA=="500") categories = {"catma175","catma300"};
+
+  for(unsigned int icat = 0; icat < categories.size(); icat++){
+    TString cat = categories[icat];
+
+
+    // create the workspace
+    CreateRooWorkspace prod("allyears",cat);
+
+
+    // muon channel: save data hist and bkg fit to the workspace
+    prod.SaveDataAndBkgFunc(defs::eSR, defs::eMuon, dodata, all_bkgds,"allyears",cat);
+
+    // electron channel: save data hist and bkg fit to the workspace
+    prod.SaveDataAndBkgFunc(defs::eSR, defs::eEle, dodata, all_bkgds, "allyears",cat);
+
+    // now produce the signals
+    prod.SaveSignals(defs::eMuon,"allyears", cat,MA);
+
+    // now produce the signals
+    prod.SaveSignals(defs::eEle,"allyears",cat,MA);  
+  
+
+    prod.PrintWorkspace();
+
+    prod.StoreWorkspace("allyears",cat,MA);
+  }
+	return 0;
+
+}
