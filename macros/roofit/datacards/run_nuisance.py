b3cde390433243215e109ea84fb01ece8732fcc6
Anna Benecke <anna.benecke@cern.ch>
2022-04-02 17:16:10 +0200

    updating to categories

--- macros/roofit/datacards/run_nuisance.py
+++ macros/roofit/datacards/run_nuisance.py
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-years="2016v3"
-masses=(1000)
-mass=1000
+
+masses=(600)
+mass=600
 
 functionused=1
 
-set_params="pdf_index_much_2016v3=${functionused},pdf_index_MT${mass}_much_2016v3=0,pdf_index_ech_2016v3=${functionused},pdf_index_MT${mass}_ech_2016v3=0,pdf_index_much_2017v2=${functionused},pdf_index_MT${mass}_much_2017v2=0,pdf_index_ech_2017v2=${functionused},pdf_index_MT${mass}_ech_2017v2=0,pdf_index_much_2018=${functionused},pdf_index_MT${mass}_much_2018=0,pdf_index_ech_2018=${functionused},pdf_index_MT${mass}_ech_2018=0"
+years=("2017v2" "2016v3" "2018")
+categories=("chi2h_2" "catma90" "catma175" "catma300" "catma60")
+funct="1"
+freeze_param=""
+set_param=""
 
-freeze_params="pdf_index_much_2016v3,pdf_index_MT${mass}_much_2016v3,pdf_index_ech_2016v3,pdf_index_MT${mass}_ech_2016v3,sg_mean_2016v3,sg_sigma_2016v3,sg_JERmeandown_2016v3,sg_JERmeanup_2016v3,sg_JECmeandown_2016v3,sg_JECmeanup_2016v3,sg_JERsigmadown_2016v3,sg_JERsigmaup_2016v3,sg_JECsigmadown_2016v3,sg_JECsigmaup_2016v3,pdf_index_much_2017v2,pdf_index_MT${mass}_much_2017v2,pdf_index_ech_2017v2,pdf_index_MT${mass}_ech_2017v2,sg_mean_2017v2,sg_sigma_2017v2,sg_JERmeandown_2017v2,sg_JERmeanup_2017v2,sg_JECmeandown_2017v2,sg_JECmeanup_2017v2,sg_JERsigmadown_2017v2,sg_JERsigmaup_2017v2,sg_JECsigmadown_2017v2,sg_JECsigmaup_2017v2,pdf_index_much_2018,pdf_index_MT${mass}_much_2018,pdf_index_ech_2018,pdf_index_MT${mass}_ech_2018,sg_mean_2018,sg_sigma_2018,sg_JERmeandown_2018,sg_JERmeanup_2018,sg_JECmeandown_2018,sg_JECmeanup_2018,sg_JERsigmadown_2018,sg_JERsigmaup_2018,sg_JECsigmadown_2018,sg_JECsigmaup_2018,sg_JERmeandown2_2018,sg_JERmeanup2_2018,sg_JECmeandown2_2018,sg_JECmeanup2_2018,sg_JERsigmadown2_2018,sg_JERsigmaup2_2018,sg_JECsigmadown2_2018,sg_JECsigmaup2_2018,sg_JERmeandown2_2017v2,sg_JERmeanup2_2017v2,sg_JECmeandown2_2017v2,sg_JECmeanup2_2017v2,sg_JERsigmadown2_2017v2,sg_JERsigmaup2_2017v2,sg_JECsigmadown2_2017v2,sg_JECsigmaup2_2017v2,sg_JERmeandown2_2016v3,sg_JERmeanup2_2016v3,sg_JECmeandown2_2016v3,sg_JECmeanup2_2016v3,sg_JERsigmadown2_2016v3,sg_JERsigmaup2_2016v3,sg_JECsigmadown2_2016v3,sg_JECsigmaup2_2016v3,sg_mean2_2016v3,sg_sigma2_2016v3,sg_mean2_2017v2,sg_sigma2_2017v2,sg_mean2_2018,sg_sigma2_2018,sg_fnorm_2018,sg_fnorm_2017v2,sg_fnorm_2016v3"
+for cat in "${categories[@]}" ; do
+for year in "${years[@]}"; do
+freeze_param="${freeze_param}sg_JERmeandown_"$year"_"$cat",sg_JERmeanup_"$year"_"$cat",sg_JECmeandown_"$year"_"$cat",sg_JECmeanup_"$year"_"$cat",sg_JERsigmadown_"$year"_"$cat",sg_JERsigmaup_"$year"_"$cat",sg_JECsigmadown_"$year"_"$cat",sg_JECsigmaup_"$year"_"$cat",sg_JERmeandown2_"$year"_"$cat",sg_JERmeanup2_"$year"_"$cat",sg_JECmeandown2_"$year"_"$cat",sg_JECmeanup2_"$year"_"$cat",sg_JERsigmadown2_"$year"_"$cat",sg_JERsigmaup2_"$year"_"$cat",sg_JECsigmadown2_"$year"_"$cat",sg_JECsigmaup2_"$year"_"$cat",sg_mean2_"$year"_"$cat","
 
+#freeze_param="${freeze_param},pdf_index_much_"$year"_"$cat",pdf_index_ech_"$year"_"$cat","
+#freeze_param="${freeze_param},pdf_index_MT${mass}_much_"$year"_"$cat",pdf_index_MT${mass}_ech_"$year"_"$cat","
+#freeze_param="${freeze_param},sg_sigma2_"$year"_"$cat",sg_sigma_"$year"_"$cat",sg_mean_"$year"_"$cat","
 
+set_param="${set_param}pdf_index_much_"$year"_"$cat"=${funct},pdf_index_ech_"$year"_"$cat"=${funct},pdf_index_MT${mass}_much_"$year"_"$cat"=0,pdf_index_MT${mass}_ech_"$year"_"$cat"=0,"
+done
+done
+
+freeze_param="${freeze_param: : -1}"
+set_param="${set_param: : -1}"
 
 year="allyears"
 
@@ -25,9 +40,9 @@ for mass in "${masses[@]}" ; do
 
 
 ##### all three years
-   eval "combineTool.py -M Impacts -d Datacard_"$year"_M$mass.root --doInitialFit --robustFit 1 -m 125  --freezeParameters ${freeze_params} --setParameters ${set_params} --rMin -3 --rMax 3 --cminDefaultMinimizerStrategy=0" # normal version --seed 8192
+   eval "combineTool.py -M Impacts -d Datacard_"$year"_M$mass.root --doInitialFit --robustFit 1 -m 125  --freezeParameters ${freeze_param} --setParameters ${set_param} --rMin -3 --rMax 3 --cminDefaultMinimizerStrategy=0" # normal version --seed 8192
 
-    eval "combineTool.py -M Impacts -d Datacard_"$year"_M$mass.root -m 125 --robustFit 1 --doFits --rMin -3 --rMax 3 --cminDefaultMinimizerStrategy=0 "
+    eval "combineTool.py -M Impacts -d Datacard_"$year"_M$mass.root -m 125 --robustFit 1 --doFits --rMin -3 --rMax 3 --cminDefaultMinimizerStrategy=0"
 
     eval "combineTool.py -M Impacts -d Datacard_"$year"_M$mass.root  -m 125 -o impacts.json"
 
